7c8870705e8d06065b7840d6353d701521878b3e
Manish  Mishra <manishweb9199@gmail.com>
2026-05-31 22:48:12 +0530

    Create palindrome02_08.java

---
+++ palindrome02_08.java
@@ -0,0 +1,10 @@
+public class Palindrome02_08 {
+  public  static void  main(String[]args){
+    String s="malayalam";
+    if(ispalindrme ())
+    System.out.println(s+"is a palindrome string");
+    else 
+    System.out.println(s+"is not a paindrome string");
+  }
+
+}
